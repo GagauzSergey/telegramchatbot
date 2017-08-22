b765b1832830cfeac420b4bd96d9e6a40cc0658b
GagauzSergey <gagauzsv@gmail.com>
2017-08-23 01:40:23 +0300

    Commit 1.3

--- src/main/java/com/telebot/smartbot/Bot.java
+++ src/main/java/com/telebot/smartbot/Bot.java
@@ -42,27 +42,33 @@ public class Bot extends TelegramLongPollingBot {
         Message message = update.getMessage();
         JsonCurrencyParser jsonCurrencyParser = new JsonCurrencyParser();
 
-        //replace with switch-case
         if (message != null && message.hasText()) {
-            if (message.getText().equals("/help")) {
-                sendNewMessage(message, "Hello, wats up");
-            } else if (message.getText().equals("How are you?")) {
-                sendNewMessage(message, "I am fine! Thank you!");
-            } else if (message.getText().equals("/time")) {
-                sendNewMessage(message, timeHelper.getTimeDate());
-            } else if (message.getText().equals("/currency")) {
-                sendNewMessage(message, jsonCurrencyParser.currencyRequest());
-            } else if (message.getText().equals("/alpha")) {
-                sendNewMessage(message, jsonCurrencyParser.getInfoFromJson(8));
-            } else if (message.getText().equals("/creditagricol")) {
-                sendNewMessage(message, jsonCurrencyParser.getInfoFromJson(35));
-            } else if (message.getText().equals("/privatebank")) {
-                sendNewMessage(message, jsonCurrencyParser.getInfoFromJson(63));
-            } else if (message.getText().equals("/ukrsibbank")) {
-                sendNewMessage(message, jsonCurrencyParser.getInfoFromJson(73));
-            } else sendNewMessage(message, "I'am sinking, wait just a second");
+            switch (message.getText()) {
+                case "/help":
+                    sendNewMessage(message, "Hello, wats up");
+                    break;
+                case "/time":
+                    sendNewMessage(message, timeHelper.getTimeDate());
+                    break;
+                case "/currency":
+                    sendNewMessage(message, jsonCurrencyParser.currencyRequest());
+                    break;
+                case "/alpha":
+                    sendNewMessage(message, jsonCurrencyParser.getInfoFromJson(8));
+                    break;
+                case "/creditagricol":
+                    sendNewMessage(message, jsonCurrencyParser.getInfoFromJson(35));
+                    break;
+                case "/privatebank":
+                    sendNewMessage(message, jsonCurrencyParser.getInfoFromJson(63));
+                    break;
+                case "/ukrsibbank":
+                    sendNewMessage(message, jsonCurrencyParser.getInfoFromJson(73));
+                    break;
+            }
         }
     }
+
 
     public void sendNewMessage(Message message, String messageText) {
         SendMessage sendMessage = new SendMessage();
